4dccf28834576f025c81767536c712fe1c506fe9
oliviaalx <47886473+oliviaalx@users.noreply.github.com>
2023-10-26 13:35:33 +0100

    feature/gradient-generator
    Adding basic gradient-generator

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,1 +1,23 @@
-alert("Under Construction :)")
+
+function ConstructionMsg(){
+  alert("Under Construction :)")
+}
+
+
+function generateGradient() {
+  var gradientBox = document.getElementById('gradient-box');
+  var colors = [
+      getRandomColor(),
+      getRandomColor()
+  ];
+  gradientBox.style.background = 'linear-gradient(45deg, ' + colors[0] + ', ' + colors[1] + ')';
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
