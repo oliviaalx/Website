2590214f2ce6e735ef346f042dd45d5b44eeffc7
oliviaalx <47886473+oliviaalx@users.noreply.github.com>
2023-10-27 14:41:47 +0100

    Changed menu

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -34,4 +34,45 @@ function getRandomColor() {
         link.download = 'gradient.png';
         link.click();
     });
-}
+
+  }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const audio = document.getElementById("myAudio");
+    const playButton = document.getElementById("playButton");
+    const previousButton = document.getElementById("previousButton");
+    const nextButton = document.getElementById("nextButton");
+
+    const playlist = [
+        "Audio/audio2.mp3",
+        "Audio/audio3.mp3",
+    ];
+
+    let currentSongIndex = 0;
+
+    playButton.addEventListener("click", function () {
+        if (audio.paused) {
+            audio.play();
+            playButton.innerHTML = '<i class="ph-bold ph-pause"></i>';
+        } else {
+            audio.pause();
+            playButton.innerHTML = '<i class="ph-bold ph-play"></i>';
+        }
+    });
+
+    previousButton.addEventListener("click", function () {
+        if (currentSongIndex > 0) {
+            currentSongIndex--;
+        }
+        audio.src = playlist[currentSongIndex];
+        audio.play();
+    });
+
+    nextButton.addEventListener("click", function () {
+        if (currentSongIndex < playlist.length - 1) {
+            currentSongIndex++;
+        }
+        audio.src = playlist[currentSongIndex];
+        audio.play();
+    });
+});
